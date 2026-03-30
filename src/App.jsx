9f45ff8e545c0406c6e54a58b63e3283a774f20d
omarakamal <omaraakamal@gmail.com>
2026-03-30 19:01:38 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router"
 import Homepage from "./pages/Homepage"
+import AllPets from "./pages/AllPets"
 function App() {
   return (
     <div>
 
       <Routes>
         <Route path='/' element={<Homepage/>} />
+        <Route path='/pets' element={<AllPets/>}/>
       </Routes>
     </div>
   )
