ff1e3e2ab6c77de0bff42078d6d558adbc46cc04
omarakamal <omaraakamal@gmail.com>
2026-03-30 19:03:16 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,4 +13,9 @@ function App() {
   )
 }
 
+// Exercise 1:
+// 1. Create a new component in the pages folder called CreatePet.jsx
+// 2. add an <h1>Create a new Pet </h1>
+// 3. have it so when we go to the /pets/create route we see this component
+
 export default App
