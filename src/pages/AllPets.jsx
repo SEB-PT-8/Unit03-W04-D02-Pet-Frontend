47e2839ef070ef81d50f75cf9f627db6b5dd2e40
omarakamal <omaraakamal@gmail.com>
2026-03-30 19:57:38 +0300

    update

--- src/pages/AllPets.jsx
+++ src/pages/AllPets.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import axios from 'axios'
 
 function AllPets() {
+    const [pets, setPets] = useState([])
+
+    async function getAllPets(){
+        // 1. get all the pets from our API
+        const allPets = await axios.get('http://localhost:3000/pets')
+        // 2. set the state to all the pets
+        console.log(allPets.data)
+        setPets(allPets.data)
+    }
+    
+    // 2 arguments
+    // arrow function
+    // dependancy array
+    useEffect(()=>{
+        getAllPets()
+    },[])
+
   return (
     <div>
         <h1>All Pets</h1>
