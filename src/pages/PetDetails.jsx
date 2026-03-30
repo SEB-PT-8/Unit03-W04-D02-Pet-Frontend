a8754763af0ca4bc469cc168552422352cdbdd6f
omarakamal <omaraakamal@gmail.com>
2026-03-30 20:12:57 +0300

    update

--- src/pages/PetDetails.jsx
+++ src/pages/PetDetails.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
+import axios from 'axios'
+
+
 
 function PetDetails() {
+    const [pet, setPet] = useState(null)
+    const {id} = useParams()
+    console.log(id)
+
+    async function getOnePet(){
+        const onePet = axios.get(`http://localhost:3000/pets/${id}`)
+
+        setPet(onePet.data)
+    }
+
+    useEffect(()=>{
+        getOnePet()
+    },[])
+
+
   return (
     <div>
         <h1>Pet Details</h1>
