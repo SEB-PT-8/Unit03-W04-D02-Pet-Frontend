2d8955bcacd13df8bce2c35c8b2c19f7237d5b0c
omarakamal <omaraakamal@gmail.com>
2026-03-31 18:34:42 +0300

    update

--- src/pages/CreatePet.jsx
+++ src/pages/CreatePet.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 function CreatePet() {
+
+
+  function handleSubmit(event){
+    event.preventDefault()
+  }
   return (
     <div>
         <h1>Create a new Pet</h1>
+
+        <form onSubmit={handleSubmit}>
+
+          <button>Create Pet</button>
+        </form>
     </div>
   )
 }
